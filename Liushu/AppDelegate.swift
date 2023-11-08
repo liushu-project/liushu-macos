f776ddf47e3a5c3042f0fc6f0df1fdb5c55c67d2
Elliot <hack00mind@gmail.com>
2023-11-08 13:36:48 +0800

    style: format swift code

--- Liushu/AppDelegate.swift
+++ Liushu/AppDelegate.swift
@@ -11,40 +11,43 @@ import LiushuCore
 
 // necessary to launch this app
 class NSManualApplication: NSApplication {
-    private let appDelegate = AppDelegate()
+  private let appDelegate = AppDelegate()
 
-    override init() {
-        super.init()
-        self.delegate = appDelegate
-    }
+  override init() {
+    super.init()
+    self.delegate = appDelegate
+  }
 
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
 }
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-    var server = IMKServer()
-    var candidatesWindow = IMKCandidates()
-    var engineAgent: InputMethodEngineAgent?
+  var server = IMKServer()
+  var candidatesWindow = IMKCandidates()
+  var engineAgent: InputMethodEngineAgent?
 
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        // Insert code here to initialize your application
-        server = IMKServer(name: Bundle.main.infoDictionary?["InputMethodConnectionName"] as? String, bundleIdentifier: Bundle.main.bundleIdentifier)
-        candidatesWindow = IMKCandidates(server: server, panelType: kIMKSingleRowSteppingCandidatePanel, styleType: kIMKMain)
+  func applicationDidFinishLaunching(_ notification: Notification) {
+    // Insert code here to initialize your application
+    server = IMKServer(
+      name: Bundle.main.infoDictionary?["InputMethodConnectionName"] as? String,
+      bundleIdentifier: Bundle.main.bundleIdentifier)
+    candidatesWindow = IMKCandidates(
+      server: server, panelType: kIMKSingleRowSteppingCandidatePanel, styleType: kIMKMain)
 
-        let dictPath = ConfigManager.shared.getDictPath()
+    let dictPath = ConfigManager.shared.getDictPath()
 
-        do {
-            engineAgent = try InputMethodEngineAgent(dictPath: dictPath)
-        } catch {
-            fatalError("could not init engine")
-        }
-        NSLog("tried connection")
+    do {
+      engineAgent = try InputMethodEngineAgent(dictPath: dictPath)
+    } catch {
+      fatalError("could not init engine")
     }
+    NSLog("tried connection")
+  }
 
-    func applicationWillTerminate(_ notification: Notification) {
-        // Insert code here to tear down your application
-    }
+  func applicationWillTerminate(_ notification: Notification) {
+    // Insert code here to tear down your application
+  }
 }
--- Liushu/ConfigManager.swift
+++ Liushu/ConfigManager.swift
@@ -8,15 +8,18 @@
 import Foundation
 
 class ConfigManager {
-    static let shared = ConfigManager()
+  static let shared = ConfigManager()
 
-    func getDictPath() -> String {
-        let appSupportURL = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first!
-        let configDirURL = appSupportURL.appendingPathComponent(Bundle.main.bundleIdentifier ?? "com.elliot00.inputmethod.Liushu")
+  func getDictPath() -> String {
+    let appSupportURL = FileManager.default.urls(
+      for: .applicationSupportDirectory, in: .userDomainMask
+    ).first!
+    let configDirURL = appSupportURL.appendingPathComponent(
+      Bundle.main.bundleIdentifier ?? "com.elliot00.inputmethod.Liushu")
 
-        try? FileManager.default.createDirectory(at: configDirURL, withIntermediateDirectories: true)
+    try? FileManager.default.createDirectory(at: configDirURL, withIntermediateDirectories: true)
 
-        let dictFileUrl = configDirURL.appendingPathComponent("sunman.trie")
-        return dictFileUrl.path(percentEncoded: false)
-    }
+    let dictFileUrl = configDirURL.appendingPathComponent("sunman.trie")
+    return dictFileUrl.path(percentEncoded: false)
+  }
 }
